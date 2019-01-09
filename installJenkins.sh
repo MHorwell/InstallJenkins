072dc28ccd338b474f20e0755fbd4dc2e5949a9e
MHorwell <mattjhorwell@gmail.com>
2019-01-09 09:09:01 +0000

    Changed commands to no longer ask for permission

--- installJenkins.sh
+++ installJenkins.sh
@@ -2,6 +2,6 @@ sudo yum install java-1.8.0-openjdk.x86_64
 cd ~ 
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install jenkins
+sudo yum -y install jenkins
 sudo systemctl start jenkins.service
 sudo systemctl enable jenkins.service
