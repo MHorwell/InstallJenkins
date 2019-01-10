c529b32cf318539e72509e6a1726bdc21e1ab463
MHorwell <mattjhorwell@gmail.com>
2019-01-10 09:09:27 +0000

    Added installation of git and Maven

--- installJenkins.sh
+++ installJenkins.sh
@@ -1,7 +1,9 @@
-sudo yum install java-1.8.0-openjdk.x86_64
+sudo yum install java-1.8.0-openjdk.x86_64 -y
 cd ~ 
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum -y install jenkins
+sudo yum install jenkins -y
 sudo systemctl start jenkins.service
 sudo systemctl enable jenkins.service
+sudo yum install git -y
+sudo yum install maven
