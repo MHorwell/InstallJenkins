9e863805f76b7ff00743717cf921a25b4c6af966
MHorwell <mattjhorwell@gmail.com>
2019-01-10 10:13:09 +0000

    Changed file so it runs from Jenkins Server

--- CreateJenkinsKey.sh
+++ CreateJenkinsKey.sh
@@ -2,8 +2,7 @@ echo "Enter Jenkins login"
 JENKINSLOGIN= read
 echo "Enter Spring login"
 SPRINGLOGIN= read
-$JENKINSLOGIN
-sudo su - root
+sudo su root
 mkdir /var/lib/jenkins/.ssh
 ssh-keygen /var/lib/jenkins/.ssh/id_rsa
 cd /var/lib/jenkins
@@ -11,11 +10,9 @@ chown -R jenkins:jenkins .ssh
 chmod 700 .ssh
 chmod 600 .ssh/*
 PUBLICKEY= cat .ssh/id_rsa.pub
-exit
-exit
 $SPRINGLOGIN
 sudo useradd jenkinssudo su - jenkinsmkdir ~/.ssh
-sudo su - jenkins
+sudo su jenkins
 mkdir ~/.ssh
 $PUBLICKEY >> authorized_keys
 chmod 700 ~/.ssh
